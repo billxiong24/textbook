480d4262d79f3baad27779761a1903f3081d194b
William Xiong <william.xiong@duke.edu>
2016-11-04 15:44:28 -0400

    temp

--- Static_Full_Version/php/buy_book.php
+++ Static_Full_Version/php/buy_book.php
@@ -1,7 +1,10 @@
 <?php
 include 'functions.php';
+include 'email.php';
 session_start();
-if(isset($_POST['bookID'])){   
+if(isset($_POST['bookID'])){
+	$book = getBook($_POST['bookID']);
+	sendBoughtEmail($book, time());
     buyBook($_SESSION['username'],$_POST['bookID']);
     echo '';
 }
