877588bc2845c6d85f923cb8a9883f4e01e0be23
Sujay Garlanka <sujay.garlanka@gmail.com>
2016-11-18 10:43:17 -0500

    minor fixes

--- Static_Full_Version/search_results.php
+++ Static_Full_Version/search_results.php
@@ -100,7 +100,7 @@ else {
                                         <div class="search-form">
                                             <form action="search_results.php" method="post">
                                                 <div class="input-group">
-                                                    <input type="text" placeholder="Search ISBN, Title, Author, or Class" name="search" class="form-control input-lg">
+                                                    <input type="text" placeholder="Search Class, Title, Author, or ISBN" name="search" class="form-control input-lg">
                                                     <div class="input-group-btn">
                                                         <button class="btn btn-lg btn-primary space" type="submit">
                                                             <i class="fa fa-search"></i>
