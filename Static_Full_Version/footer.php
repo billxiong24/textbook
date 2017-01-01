e6f3fe3dc45159d06f0b1f368b9cc9ea83ede54e
Sujay Garlanka <sujay.garlanka@gmail.com>
2017-01-01 14:13:41 -0500

    add footer

---
+++ Static_Full_Version/footer.php
@@ -0,0 +1,7 @@
+<?php 
+echo '          <br>
+                <div class="footer">
+                    <span><a href="about_us.php" class="footer-tag">About us</a></span>
+                    <span class="pull-right"><a href="mailto:dukeexchange@gmail.com" class="footer-tag">Contact us</a></span>
+                </div>';
+?>
