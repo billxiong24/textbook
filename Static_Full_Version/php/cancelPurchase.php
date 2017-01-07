06ffb7e6d8cf565e1ad7ffdbe166c9f66f0390bb
root <root@localhost.localdomain>
2017-01-07 02:43:42 -0500

    path config

--- Static_Full_Version/php/cancelPurchase.php
+++ Static_Full_Version/php/cancelPurchase.php
@@ -2,7 +2,7 @@
 //include 'email.php'; // functions.php and session_start included in email.php
 //include '../model/ProductManager.class.php';
 //include '../model/TransactionManager.class.php';
-require_once("./controller/BookController.class.php");
+require_once("BookController.class.php");
 session_start();
 if(isset($_POST['purchaseID']) && isset($_SESSION['username'])){
     $_SESSION['book_controller']->cancelPurchase($_POST['purchaseID']);
